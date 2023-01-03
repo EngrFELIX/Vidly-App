15be4c11896a26b53171eaeb35618ee33b9202e0
Felix Oyanoghafo <lyxrox@gmail.com>
2023-01-03 16:21:50 +0100

    my first video rental app

---
+++ database/genres.js
@@ -0,0 +1,74 @@
+const genres = [
+    {
+        id: 1,
+        name: 'Adventure',
+        description: 'This is a journey that often takes place in exotic locations and can involve exploration or a quest. While there is often action in this type of movie, it is not the main emphasis. Examples of adventure movies: Indiana Jones, Pirates of the Caribbean, Tomb Raider',
+        availableMovies: 15
+    },
+
+    {
+        id: 2,
+        name: 'Animated',
+        description: 'Instead of real actors on the screen, drawn images (nowadays computer generated ones) are used to tell the story. Real actors are used for the voices of the animated characters. Examples of animated movies: Toy Story, Shrek, Coco, Frozen',
+        availableMovies: 15
+    },
+    
+    {
+        id: 3,
+        name: 'Comedy',
+        description: 'The main idea of comedies is to make the audience laugh. The main characters are involved in funny situations. Examples of comedy movies: Monty Python, The Hangover, What men want',
+        availableMovies: 15
+    },
+
+    {
+        id: 4,
+        name: 'Drama',
+        description: 'These films are serious and focus on the emotions of realistic characters and the conflicts they have. Examples of drama movies: The Godfather, Marriage Story, The Sound of Metal',
+        availableMovies: 20
+    },
+
+    {
+        id: 5,
+        name: 'Historical',
+        description: 'These films can be based on real events in the past or sometimes just the time period is used as the setting and all events are fictitious. In both cases, the narrative takes place inside an accurate depiction of a historical setting. Sometimes these are biographical movies, also known as Biopics. Examples of historical movies: Braveheart, Amadeus, Dunkirk',
+        availableMovies: 20
+    },
+
+    {
+        id: 6,
+        name: 'Horror',
+        description: 'The objective of a horror movie is to scare the viewer and if possible, make them scream (often done with a jump scare). Horror movies often feature a monster, some type of evil entity or a terrible/frightening situation. Examples of horror movies: Nightmare on Elm Street, The Exorcist, Annabelle',
+        availableMovies: 10
+    },
+
+    {
+        id: 7,
+        name: 'musical',
+        description: 'These are movies where songs are sung by the characters as a part of the narrative and usually help advance the plot or develop the film’s characters. Sometimes there are dance routines to accompany the songs. Examples of musical films: La La Land, Grease, Mamma Mia',
+        availableMovies: 15
+    },
+
+    {
+        id: 8,
+        name: 'Romance',
+        description: 'Romance films are love stories that focus on the romantic involvement of the main characters and the development of their relationship. A romantic movie that is also funny is called a Rom-Com (Romantic Comedy). Examples of romance movies: An Officer and a Gentleman, The Notebook, The Lake House',
+        availableMovies: 20
+    },
+
+    {
+        id: 9,
+        name: 'Science Fiction',
+        description: 'Also known as Sci-Fi, these are fictional stories based on science. They can be about the future or other worlds, and often include spaceships, extra-terrestrial beings (aliens), and advanced technology we don’t currently have. Examples of science fiction movies: Star Wars, Blade Runner, Avatar',
+        availableMovies: 10
+    },
+
+    {
+        id: 10,
+        name: 'Thriller',
+        description: 'A thriller film evokes excitement and suspense in the audience. A common theme in thrillers involves innocent people dealing with a deranged adversary.  Examples of thrillers: The Silence of the Lambs, The Sixth Sense, The Da Vinci Code, Shutter Island',
+        availableMovies: 10
+    },
+    
+]
+
+module.exports = genres
---
+++ database/index.js
@@ -0,0 +1,6 @@
+const genres = require('./genres')
+const movies = require('./movies')
+const users = require('./users')
+
+
+module.exports = {genres, movies, users}
