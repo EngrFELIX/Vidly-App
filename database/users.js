3764f762ab3fd45491f10a94ff8ebb51dd9ebdbb
Felix Oyanoghafo <lyxrox@gmail.com>
2023-01-05 21:34:49 +0100

    modification and second commit

--- database/users.js
+++ database/users.js
@@ -1,6 +1,75 @@
-const genres = require('./genres')
-const movies = require('./movies')
-const users = require('./users')
-
-
-module.exports = {genres, movies, users}
+const users = [
+    {
+        id: 1,
+        name: 'Tom Cruise',
+        moviesRented: [
+            { moviesId: 4, dateRented: new Date, returned: false },
+            { moviesId: 9, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Woodruff',
+        moviesRented: [
+            { moviesId: 4, dateRented: new Date, returned: false },
+            { moviesId: 7, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Osayande',
+        moviesRented: [
+            { moviesId: 1, dateRented: new Date, returned: false },
+            { moviesId: 3, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 4,
+        name: 'Denze Washington',
+        moviesRented: [
+            { moviesId: 6, dateRented: new Date, returned: false },
+            { moviesId: 8, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 5,
+        name: 'Felix',
+        moviesRented: [
+            { moviesId: 1, dateRented: new Date, returned: false },
+            { moviesId: 2, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 6,
+        name: 'Smith',
+        moviesRented: [
+            { moviesId: 1, dateRented: new Date, returned: false },
+            { moviesId: 3, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 7,
+        name: 'Marlon',
+        moviesRented: [
+            { moviesId: 3, dateRented: new Date, returned: false },
+            { moviesId: 5, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 8,
+        name: 'Johnny',
+        moviesRented: [
+            { moviesId: 6, dateRented: new Date, returned: false },
+            { moviesId: 1, dateRented: new Date, returned: true },
+        ]
+    },
+    {
+        id: 9,
+        name: 'Joyner',
+        moviesRented: [
+            { moviesId: 10, dateRented: new Date, returned: false },
+            { moviesId: 1, dateRented: new Date, returned: true },
+        ]
+    },
+]
+module.exports = users;
